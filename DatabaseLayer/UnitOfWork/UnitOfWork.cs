2863f2c39474c76a463a0b9d5efd47ea4bbec19b
ayaahmedswe544 <aya.ahmed.swe@gmail.com>
2026-07-09 01:58:52 +0300

    unit of work

---
+++ DatabaseLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Domain.IUnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLayer.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Domain/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Domain.IUnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        Task<int> SaveChangesAsync();
+
+    }
+}
