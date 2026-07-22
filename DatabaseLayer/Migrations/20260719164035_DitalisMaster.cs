c1e7613e492c7fe35879d0ba432f1a656040ee25
hegazy <abdohegazyhelwan@gmail.com>
2026-07-22 14:37:35 +0300

    AppotmentS

--- DatabaseLayer/Migrations/20260719164035_DitalisMaster.cs
+++ DatabaseLayer/Migrations/20260719164035_DitalisMaster.cs
@@ -11,26 +11,25 @@ namespace DatabaseLayer.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "diagnos");
+            migrationBuilder.DropTable( name: "diagnos");
 
-            migrationBuilder.CreateTable(
+            migrationBuilder.CreateTable(   
                 name: "DiagnosMaster",
                 columns: table => new
                 {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Id          = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name        = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Code = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    IsUpdated = table.Column<bool>(type: "bit", nullable: false),
-                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
-                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                    Code        = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CreatedAt   = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt   = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsUpdated   = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted   = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt   = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy   = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy   = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedBy   = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsActive    = table.Column<bool>(type: "bit", nullable: false)
                 },
                 constraints: table =>
                 {
--- DatabaseLayer/Repository/AppoinmentRepository.cs
+++ DatabaseLayer/Repository/AppoinmentRepository.cs
@@ -1,13 +1,34 @@
-﻿using Domain.IRepository;
-using Domain.Models;
+﻿using Domain.Models;
+using Domain.IRepository;
 
 namespace DatabaseLayer.Repository;
 
 public class AppoinmentRepository : BaseRepository<Appointment>, IAppoinmentRepository
 {
-    
-    public AppoinmentRepository(AppDbContext context) : base(context)
+  
+    public AppoinmentRepository(AppDbContext _context) : base(context)
     {
-        
+      
     }
+    
+    public  async Task<List<Appointment>> GetByPatientId(Guid PatientId)
+    {
+        var x = await FindAllAsync(s => s.PatientId == PatientId);
+        return x.ToList();
+    }
+
+
+    public async Task<List<Appointment>> GetByDoctorId(Guid DoctorId)
+    {
+        var x = await FindAllAsync(s => s.DoctorId == DoctorId);
+        return x.ToList();
+    }
+
+
+    public async  Task<List<Appointment>> GetByCreatby(Guid CreatedBy)
+    {
+        var x = await FindAllAsync(s => s.CreatedBy == CreatedBy);
+        return x.ToList();
+    }
+
 }
