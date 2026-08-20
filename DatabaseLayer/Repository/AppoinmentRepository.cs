fbda45bf64a17c177b9847cb040127320cd5b8b8
abdo <abdohegazyhelwan@gmial.com>
2026-08-20 14:18:30 +0300

    vission2

--- DatabaseLayer/Repository/AppoinmentRepository.cs
+++ DatabaseLayer/Repository/AppoinmentRepository.cs
@@ -11,6 +11,7 @@ public class AppoinmentRepository : BaseRepository<Appointment>, IAppoinmentRepo
     public  async Task<List<Appointment>> GetByPatientId(Guid PatientId)
     {
         var x = await FindAllAsync(s => s.PatientId == PatientId && s.Status != "Completed", new string[] { "Patient", "Doctor" });
+            x = x.OrderByDescending(m=> m.AppointmentDate);
         return x.ToList();
     }
 
@@ -18,6 +19,7 @@ public class AppoinmentRepository : BaseRepository<Appointment>, IAppoinmentRepo
     public async Task<List<Appointment>> GetByDoctorId(Guid DoctorId)
     {
         var x = await FindAllAsync(s => s.DoctorId == DoctorId && s.Status == "Pending", new string[] { "Patient", "Doctor" });
+        x = x.OrderByDescending(m=> m.AppointmentDate);
         return x.ToList();
     }
 
@@ -25,6 +27,7 @@ public class AppoinmentRepository : BaseRepository<Appointment>, IAppoinmentRepo
     public async  Task<List<Appointment>> GetByCreatby(Guid CreatedBy)
     {
         var x = await FindAllAsync(s => s.CreatedBy == Convert.ToString(CreatedBy), new string[] { "Patient", "Doctor" });
+            x = x.OrderByDescending(m=> m.AppointmentDate);
         return x.ToList();
     }
 
@@ -34,4 +37,5 @@ public class AppoinmentRepository : BaseRepository<Appointment>, IAppoinmentRepo
         x = x.OrderByDescending(m => m.AppointmentDate);
         return x.ToList();
     }
+    
 }
